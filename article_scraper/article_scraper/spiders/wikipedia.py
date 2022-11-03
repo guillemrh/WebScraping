63d5dc882db66807dbdc142de2a3898df4251ce7
guillemrh <guillemmm.98@gmail.com>
2022-11-03 16:25:10 +0100

    save data to csv

--- article_scraper/article_scraper/spiders/wikipedia.py
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -1,17 +1,21 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
+from article_scraper.items import Article
 
 class WikipediaSpider(CrawlSpider):
     name = 'wikipedia'
     allowed_domains = ['en.wikipedia.org']
-    start_urls = ['http://en.wikipedia.org/wiki/Kevin_Bacon']
-    
-    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+    start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
+
+    rules = [
+        Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)
+    ]
+
     def parse_info(self, response):
-        return{
-            'title': response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get(),
-            'url': response.url,
-            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
-        }
+        article = Article()
+        article['title']= response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()')
+        article['url'] = response.url
+
+        article['lastUpdated'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        return article
