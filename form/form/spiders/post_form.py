3b5dfa9a753483c8128634393657e892d07fedc8
guillemrh <guillemmm.98@gmail.com>
2022-11-11 16:08:20 +0100

    post_form

---
+++ form/form/spiders/post_form.py
@@ -0,0 +1,18 @@
+import scrapy
+
+from scrapy.http import FormRequest
+
+class GetFormSpider(scrapy.Spider):
+    name = 'post_form'
+    allowed_domains = ['pythonscraping.com']
+
+    def start_requests(self):
+        names = ['Josep', 'Inés', 'Gisela']
+        quests = ['to seek the grail', 'to learn Python', 'to scrape the web']
+        return [FormRequest(
+            'http://pythonscraping.com/linkedin/formAction2.php',
+            formdata={'name': name, 'quest': quest, 'color': 'blue'},
+            callback=self.parse)  for name in names for quest in quests]
+
+    def parse(self, response):
+        return {'text': response.xpath('//div[@class="wrapper"]/text()').get()}
